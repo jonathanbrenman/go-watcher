5b8169d41398904a877203a7b620cc785f8da018
Jonathan Brenman <jonathanbrenman@gmail.com>
2021-05-29 23:57:55 -0300

    welcome

--- main.go
+++ main.go
@@ -1,13 +1,27 @@
 package main
 
 import (
+	"fmt"
 	"github.com/spf13/cobra"
 	"go-watcher/cmd"
+	"log"
 )
 
 func main() {
+	welcome := `                                  __         .__                  
+   ____   ____   __  _  _______ _/  |_  ____ |  |__   ___________ 
+  / ___\ /  _ \  \ \/ \/ /\__  \\   __\/ ___\|  |  \_/ __ \_  __ \
+ / /_/  >  <_> )  \     /  / __ \|  | \  \___|   Y  \  ___/|  | \/
+ \___  / \____/    \/\_/  (____  /__|  \___  >___|  /\___  >__|   
+/_____/                        \/          \/     \/     \/       `
+
+	fmt.Println(welcome)
+	fmt.Println()
+
 	var rootCmd = &cobra.Command{Use: "app"}
 	rootCmd.AddCommand(cmd.WatchCmd)
 	rootCmd.AddCommand(cmd.ListCmd)
-	rootCmd.Execute()
+	if err := rootCmd.Execute(); err != nil {
+		log.Fatal(err)
+	}
 }
