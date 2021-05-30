b2c038eefb0599b599123dd04dd2240109dea7cb
Jonathan Brenman <jonathanbrenman@gmail.com>
2021-05-29 23:46:07 -0300

    fix

--- cmd/watch.go
+++ cmd/watch.go
@@ -3,7 +3,9 @@ package cmd
 import (
 	"github.com/spf13/cobra"
 	"go-watcher/watcher"
+	"log"
 	"strings"
+	"time"
 )
 
 var WatchCmd = &cobra.Command{
@@ -14,14 +16,17 @@ var WatchCmd = &cobra.Command{
 
 func init() {
 	// Flags cmd watch here
-	WatchCmd.Flags().String("path", "./","Directory to watch")
-	WatchCmd.Flags().Duration("delay", 3,"Delay for next execution")
+	WatchCmd.Flags().String("path", "","Directory to watch")
+	WatchCmd.Flags().Duration("delay", 3 * time.Second,"Delay for next execution")
 	WatchCmd.Flags().Bool("debug", false,"Debug will give more info about the process")
 	WatchCmd.Flags().String("filters", "","Extension to filter with comma separated")
 }
 
 func watchCmd(cmd *cobra.Command, args []string) {
 	path, _ := cmd.Flags().GetString("path")
+	if path == "" {
+		log.Fatal("missing path flag.")
+	}
   	delay, _ := cmd.Flags().GetDuration("delay")
   	debug, _ := cmd.Flags().GetBool("debug")
   	filters, _ := cmd.Flags().GetString("filters")
